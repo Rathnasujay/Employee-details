ae6151cd611b2f8315c7f6f1d5cb56355d07ccf7
Rathnasujay <rathnasujayuidesigner@gmail.com>
2023-04-28 17:08:23 +0530

    code pushed

---
+++ src/org/det/Client.java
@@ -0,0 +1,30 @@
+package org.det;
+
+import java.util.Scanner;
+
+public class Client {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter the age");
+	byte age = s.nextByte();
+	System.out.println("Enter the phno");
+	long phno = s.nextLong();
+	System.out.println();
+	double sky = s.nextDouble();
+	System.out.println("enter your name");
+	String next = s.next();
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/org/det/EmployeeDetails.java
@@ -0,0 +1,27 @@
+package org.det;
+
+import java.util.Scanner;
+
+public class EmployeeDetails {
+public static void main(String[] args) {
+	Scanner v = new Scanner(System.in);
+	System.out.println("Enter your name");
+	String name = v.next();
+	System.out.println("Enter your age");
+	byte age = v.nextByte();
+	System.out.println("Sky is blue");
+	boolean sky = v.nextBoolean();
+System.out.println("Name is :"+name);
+System.out.println("Age is :"+age);
+System.out.println("Sky is blue :"+sky);
+
+
+
+}
+	
+	
+	
+	
+	
+	
+}
